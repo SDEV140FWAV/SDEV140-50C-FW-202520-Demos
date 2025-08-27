fc9bfed86cc27db01d9c0a5d9ff8d0ff195f1fd6
Angela Venable <avenable11@ivytech.edu>
2025-08-27 19:29:56 -0400

    miles per gallon program without input

---
+++ 08_27_25/demo1.py
@@ -0,0 +1,11 @@
+'''
+Demo 1 input and output
+Author: Angela Venable
+'''
+
+print("Hello World!")
+""
+print('the quick brown \n\tfox\b3')
+';'
+'432idodsaeasd'
+
---
+++ 08_27_25/milesPerGallon.py
@@ -0,0 +1,16 @@
+'''
+Program: Calculate Miles per gallon
+Author: Angela Venable
+'''
+
+starting_mileage = 300
+ending_mileage = 500
+gallons_used = 10
+distance_driven = ending_mileage - starting_mileage
+miles_per_gallon = distance_driven / gallons_used
+print("Starting Mileage: ",end=" ")
+print(starting_mileage)
+print("Ending Mileage: ", ending_mileage)
+print("Distance Driven: ", distance_driven)
+print("Gallons Used: ", gallons_used)
+print("Miles Per Gallon: ", miles_per_gallon)
