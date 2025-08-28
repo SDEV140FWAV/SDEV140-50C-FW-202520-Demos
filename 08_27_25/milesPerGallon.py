4699224572ac30855c40dfc838c750019a3da6b7
Angela Venable <avenable11@ivytech.edu>
2025-08-27 20:06:00 -0400

    miles per gallon program with input

--- 08_27_25/milesPerGallon.py
+++ 08_27_25/milesPerGallon.py
@@ -3,9 +3,9 @@ Program: Calculate Miles per gallon
 Author: Angela Venable
 '''
 
-starting_mileage = 300
-ending_mileage = 500
-gallons_used = 10
+starting_mileage = int(input("Enter the starting mileage: "))
+ending_mileage = int(input("Enter the ending mileage: "))
+gallons_used = int(input("Enter the gallons used: "))
 distance_driven = ending_mileage - starting_mileage
 miles_per_gallon = distance_driven / gallons_used
 print("Starting Mileage: ",end=" ")
