b99603c976ae318504f6020eb66a5de5d9bfd1e3
Angela Venable <avenable11@ivytech.edu>
2025-09-03 18:50:33 -0400

    madlib

---
+++ 09_03_25/madlib.py
@@ -0,0 +1,40 @@
+'''
+Mad lib program
+https://madlibs.com/wp-content/uploads/2024/06/VacationFun_ML_2009_pg15.pdf
+Author: Angela Venable
+'''
+
+ADJECTIVE = "Enter an adjective: "
+NOUN = "Enter a noun: "
+PLURAL_NOUN = "Enter a plural noun: "
+GAME = "Enter a game: "
+ING_VERB = "Enter a verb ending in ing: "
+PLANT = "Enter a plant: "
+BODY_PART = "Enter a part of the body: "
+PLACE = "Enter a place: "
+NUMBER = "Enter a number: "
+
+
+adjective1 = input(ADJECTIVE)
+adjective2 = input(ADJECTIVE)
+noun1 = input(NOUN)
+noun2 = input(NOUN)
+plural_noun1 = input(PLURAL_NOUN)
+game = input(GAME)
+plural_noun2 = input(PLURAL_NOUN)
+verb1 = input(ING_VERB)
+verb2 = input(ING_VERB)
+plural_noun3 = input(PLURAL_NOUN)
+verb3 = input(ING_VERB)
+noun3 = input(NOUN)
+plant = input(PLANT)
+body_part = input(BODY_PART)
+place = input(PLACE)
+verb4 = input(ING_VERB)
+adjective3 = input(ADJECTIVE)
+number = input(NUMBER)
+plural_noun4 = input(PLURAL_NOUN)
+
+print(f"A vacation is when you take a trip to some {adjective1} place with your {adjective2} family.")
+
+
