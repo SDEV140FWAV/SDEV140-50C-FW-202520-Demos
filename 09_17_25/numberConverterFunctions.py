d385a6599776edbfcaf39b90c1c80aa62a3b0b2c
Angela Venable <avenable11@ivytech.edu>
2025-09-17 20:53:22 -0400

    added module check

--- 09_17_25/numberConverterFunctions.py
+++ 09_17_25/numberConverterFunctions.py
@@ -81,78 +81,82 @@ def convert_decimal_bin(decimal_num):
         decimal_num = decimal_num // 2
     return binary_number
 
+def convert_decimal_hex(decimal_num):
+    binary_number = convert_decimal_bin(decimal_num)
+    hex_number = convert_bin_hex(binary_number)
+    return hex_number
+
 def do_conversion(prompt,valid_func,convert_func):
     valid_input = False
     while not valid_input:
         user_input = input(prompt)
         valid_input = valid_func(user_input)
     
-    return convert_func(user_input)
+    return (user_input,convert_func(user_input))
 
 # To-do finish function
-def get_decimal_number(prompt):
-    return 17
+def convert_to_float_safe(string):
+    if string.count('.') > 1:
+        return None
+    if string.count('-') > 1:
+        return None
+    if string.count('-') == 1 and string[0] != '-':
+        return None
+    if string.count('+') > 1:
+        return None
+    if string.count('+') == 1 and string[0] != '+':
+        return None
+    string_cpy = string[:]
+    string_cpy = string_cpy.replace('+', "")
+    string_cpy = string_cpy.replace('-', "")
+    string_cpy = string_cpy.replace('.', "")
+    if not string_cpy.isdigit():
+        return None
+    return float(string)
+
+def test_float():
+    print(convert_to_float_safe("13.278"))
+    print(convert_to_float_safe("192.168.1.1"))
+    print(convert_to_float_safe("-12.22"))
+    print(convert_to_float_safe("12-25-2025"))
+    print(convert_to_float_safe("12 2321 jkljl"))
 
 
 
 
+def main():
+    prompt = "What operation do you want to do?\n" \
+        + "1. Convert Binary to Hexadecimal\n" \
+        + "2. Convert Hexadecimal to Binary\n" \
+        + "3. Convert Decimal to Binary\n" \
+        + "4. Convert Binary to Decimal\n"\
+        + "5. Convert Decimal to Hexadecimal\n"
+    valid_operation = False
+    while not valid_operation:
+        operation = input(prompt)
+        if operation != "1" and operation != "2" and operation != "3" and operation != "4" and operation != "5":
+            continue
+        else:
+            valid_operation = True
+    if operation == "1": 
+        binary_number, hex_number = do_conversion(prompt="Enter a binary number to convert to hexadecimal: ",valid_func=valid_binary_number, convert_func=convert_bin_hex)
+        print(f"{binary_number} is {hex_number} in hexadecimal")
 
+    elif operation == "2":    
+        hex_number, binary_number = do_conversion(prompt="Enter a hexadecimal number: ", convert_func=convert_hex_bin, valid_func=valid_hexadecimal_number)
+        print(f"{hex_number} in binary is {binary_number}")
 
-prompt = "What operation do you want to do?\n" \
-       + "1. Convert Binary to Hexadecimal\n" \
-       + "2. Convert Hexadecimal to Binary\n" \
-       + "3. Convert Decimal to Binary\n" \
-       + "4. Convert Binary to Decimal\n"
-valid_operation = False
-while not valid_operation:
-    operation = input(prompt)
-    if operation != "1" and operation != "2" and operation != "3" and operation != "4":
-        continue
-    else:
-        valid_operation = True
-if operation == "1":
-    binary_number = ""
-    hex_number = do_conversion(prompt="Enter a binary number to convert to hexadecimal: ",valid_func=valid_binary_number, convert_func=convert_bin_hex)
-    
-    print(f"{binary_number} is {hex_number} in hexadecimal")
-
-elif operation == "2":    
-    valid_num = False
-    while not valid_num:
-        hex_number = input("Enter a hexadecimal number: ")
-        valid_num = True
-        for digit in hex_number:
-            if digit.upper() not in hex_digits:
-                print(f"The input contains non hex digits: {digit}")
-                valid_num = False
-                break
-
-    binary_number = ""
-    for digit in hex_number:
-        binary_number = binary_number + hex_digits[digit.upper()]
-
-    print(f"{hex_number} in binary is {binary_number}")
-elif operation == "3":
-    valid_decimal = False
-    while not valid_decimal:
-        decimal_num = input("Enter a decimal number: ")
-        decimal_num = decimal_num.replace("-", "")
-        decimal_num = decimal_num.replace("+", "")
-        valid_decimal = True
-        for digit in decimal_num:
-            if not digit.isdigit():
-                valid_decimal = False
-                print(f"There are non decimal digits: {digit}")
-        decimal_num_str = decimal_num[:]
-        decimal_num = int(decimal_num)
-        binary_number = ""
-        while decimal_num != 0:
-            binary_number = str(decimal_num % 2) + binary_number
-            decimal_num = int(decimal_num / 2)
+    elif operation == "3":
+        decimal_num_str, binary_number = do_conversion("Enter a decimal number: ", valid_decimal_number, convert_decimal_bin)   
         print(f"{decimal_num_str} is {binary_number} in binary")
-elif operation == "4":
-    binary_number = get_binary_number("Enter a binary number to convert to decimal: ")
-    decimal_num = 0
-    for exponent in range(0, len(binary_number)):
-        decimal_num = decimal_num + int(binary_number[-1 - exponent]) * 2**exponent
-    print(f"{binary_number} is {decimal_num} in decimal")
+
+    elif operation == "4":
+        binary_number, decimal_num = do_conversion("Enter a binary number to convert to decimal: ", valid_binary_number, convert_bin_decimal)
+        print(f"{binary_number} is {decimal_num} in decimal")
+    elif operation == "5":
+        decimal_num, hex_number = do_conversion("Enter a decimal number: ", valid_decimal_number, convert_decimal_hex)
+        print(f"{decimal_num} is {hex_number} in hexadecimal")
+
+if __name__ == "__main__":
+    main()
+
